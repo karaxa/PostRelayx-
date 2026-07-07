cba9e8923a47794af676d4c95afcf075691a8349
karaxa <117860103+karaxa@users.noreply.github.com>
2026-07-07 21:20:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # Çevre Değişkenleri
 TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
-RSS_URL = "https://rss.app/feeds/drnuUpbwwO7l3hBc.xml"
+RSS_URL = "https://rss.app/feeds/42A416Kp8xiDx9yV.xml"
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(url, key)
